cfb0955b0f14ebbbb73f2bdc0a051b3219ab8734
zhycn <qizhaohong_sh@qq.com>
2026-02-10 09:38:08 +0800

    refactor: enhance global types and index page to include search configuration and improve data handling

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,6 +2,9 @@
 declare global {
   interface Window {
     toggleTheme: () => void;
+    __TOOLS__: import('../types/tool').Tool[];
+    __UI_CONFIG__: typeof import('../types/settings').UI_CONFIG;
+    __SEARCH_CONFIG__: typeof import('../types/settings').SEARCH_CONFIG;
   }
 }
 
